3f51bddb797bf538aae1a41ceecc03e29ee6a1d0
Joao Victor <joaovictoralves087@gmail.com>
2025-11-11 18:39:44 -0300

    Commit Inicial

---
+++ src/main/java/com/smartdesk/api/SmartDeskApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartdesk.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDeskApiApplication.class, args);
+	}
+
+}
